b610b22814a1915bd02c2bc9eb8edbef87af56b8
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-22 18:06:25 +0900

    Revert "finished part 9"
    This reverts commit 8ab1063f7536e003113a6d1f8b0562de81395a19.

--- app/Http/Livewire/Frontpage.php
+++ app/Http/Livewire/Frontpage.php
@@ -3,7 +3,6 @@
 namespace App\Http\Livewire;
 
 use App\Models\Page;
-use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Frontpage extends Component
@@ -36,30 +35,8 @@ class Frontpage extends Component
         $this->content = $data->content;
     }
 
-    public function sideBarLinks()
-    {
-        return DB::table('navigation_menus')
-            ->where('type', '=', 'SidebarNav')
-            ->orderBy('sequence', 'asc')
-            ->orderBy('created_at', 'asc')
-            ->get();
-    }
-
-    public function topNavLinks()
-    {
-        return DB::table('navigation_menus')
-            ->where('type', '=', 'topNav')
-            ->orderBy('sequence', 'asc')
-            ->orderBy('created_at', 'asc')
-            ->get();
-    }
-
-
     public function render()
     {
-        return view('livewire.frontpage', [
-            'sideBarLinks' => $this->sideBarLinks(),
-            'topNavLinks' => $this->topNavLinks(),
-        ])->layout('layouts.frontpage');
+        return view('livewire.frontpage')->layout('layouts.frontpage');
     }
 }
